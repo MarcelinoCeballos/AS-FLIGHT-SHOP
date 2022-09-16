613435c656c7133d07669bbe770bc7aa55d72ec3
Marcelino Ceballos <104146917+MarcelinoCeballos@users.noreply.github.com>
2022-09-16 10:17:40 -0300

    1er Entrega final. Corrección imágenes

--- src/utils/productos.js
+++ src/utils/productos.js
@@ -6,7 +6,7 @@ const productos = [
     price: 75,
     description:
       "Charreteras lavables, doradas, sin broches para colocar en el porta charreteras de su camisa",
-    image: "./assets/img/CaptainBars.png",
+    image: "/assets/img/CaptainBars.png",
     categoryId: 1,
   },
   {
@@ -16,7 +16,7 @@ const productos = [
     price: 450,
     description:
       "Impermeable, durable, interior naranja SOS, color verde militar",
-    image: "./assets/img/FlightJacket.png",
+    image: "/assets/img/FlightJacket.png",
     categoryId: 1,
   },
   {
@@ -26,7 +26,7 @@ const productos = [
     price: 75,
     description:
       "Anti reflejo, livianas, porque volar seguros no es una opción sino una necesidad",
-    image: "./assets/img/Anteojos.png",
+    image: "/assets/img/Anteojos.png",
     categoryId: 1,
   },
   {
@@ -36,7 +36,7 @@ const productos = [
     price: 950,
     description:
       "El auricular David Clark H10-13X tiene un rendimiento silencioso donde escuchará y sentirá la diferencia de inmediato.",
-    image: "./assets/img/HeadSet.png",
+    image: "/assets/img/HeadSet.png",
     categoryId: 2,
   },
   {
@@ -46,7 +46,7 @@ const productos = [
     price: 150,
     description:
       "La multiherramienta Leatherman es el instrumento indicado para cuando no pueda realizar una tarea con sus propias manos",
-    image: "./assets/img/Multitool.png",
+    image: "/assets/img/Multitool.png",
     categoryId: 2,
   },
   {
@@ -56,7 +56,7 @@ const productos = [
     price: 250,
     description:
       "La mejor herramienta para saber donde estoy, a dónde quiero ir y qué debo hacer para llegar",
-    image: "./assets/img/GPS.png",
+    image: "/assets/img/GPS.png",
     categoryId: 2,
   },
   {
@@ -65,7 +65,7 @@ const productos = [
     stock: 2,
     price: 75,
     description: "POH manual",
-    image: "./assets/img/C210.png",
+    image: "/assets/img/C210.png",
     categoryId: 3,
   },
   {
@@ -74,7 +74,7 @@ const productos = [
     stock: 1,
     price: 450,
     description: "POH manual",
-    image: "./assets/img/C172.png",
+    image: "/assets/img/C172.png",
     categoryId: 3,
   },
   {
@@ -84,7 +84,7 @@ const productos = [
     price: 75,
     description:
       "Todo lo que debés saber para aprobar los contenidos del examen de PPA",
-    image: "./assets/img/PPA.png",
+    image: "/assets/img/PPA.png",
     categoryId: 3,
   },
   {
@@ -94,7 +94,7 @@ const productos = [
     price: 75,
     description:
       "Todo lo que debés saber para ser un instructor competente que eduque con los más altos estándares",
-    image: "./assets/img/Instructor.png",
+    image: "/assets/img/Instructor.png",
     categoryId: 3,
   },
 ];
